bd91b5c2d782c02e0b52f48c371b9847e9f40e1d
kaur7141 <50632416+kaur7141@users.noreply.github.com>
2019-05-21 12:27:33 -0400

    Update Student.java

--- src/ca/sheridancollege/week1/softwarefundamentals/Student.java
+++ src/ca/sheridancollege/week1/softwarefundamentals/Student.java
@@ -14,7 +14,7 @@ public class Student {
     private String name;
     private int sID;
     private String lastName;
-
+    private String firstName;
     /**
      * Get the value of lastName
      *
