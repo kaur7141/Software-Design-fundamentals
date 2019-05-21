69c08cb70617f1269534d132e0670c33119b90f6
kaur7141 <khushpreetkaur.013@hotmail.com>
2019-05-21 13:07:28 -0400

    partTime

---
+++ src/ca/sheridancollege/week1/softwarefundamentals/partTime.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ca.sheridancollege.week1.softwarefundamentals;
+
+/**
+ *
+ * @author khush
+ */
+public class partTime {
+    
+    private String noClasses;
+
+    /**
+     * Get the value of noClasses
+     *
+     * @return the value of noClasses
+     */
+    public String getNoClasses() {
+        return noClasses;
+    }
+
+    /**
+     * Set the value of noClasses
+     *
+     * @param noClasses new value of noClasses
+     */
+    public void setNoClasses(String noClasses) {
+        this.noClasses = noClasses;
+    }
+
+}
